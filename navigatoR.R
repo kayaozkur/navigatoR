ee779c04f8b534e69815256db88d9ea2271c77ef
Kaya Ozkur <kayaozkur@gmail.com>
2023-07-31 02:55:44 -0500

    Added sub_menu function

--- navigatoR.R
+++ navigatoR.R
@@ -13,6 +13,20 @@ main_menu <- function() {
   )
 }
 
+# Sub Menu Function ----
+
+# Define Sub Menu
+sub_menu <- function() {
+  sub_options <- c("Feather Files", "Back")
+  sub_selection <- menu(sub_options, title = "Select a File Type:")
+  
+  # Define Options in the Sub Menu
+  selected_option <- switch(
+    sub_selection,
+    feather_menu(),
+    return(main_menu())  
+  )
+}
 
 
 
