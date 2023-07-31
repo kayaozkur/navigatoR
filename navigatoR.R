d172300c109e393cda32831fed76f2bfcbf9da51
Kaya Ozkur <kayaozkur@gmail.com>
2023-07-31 03:02:59 -0500

    Added feather_menu function

--- navigatoR.R
+++ navigatoR.R
@@ -28,5 +28,52 @@ sub_menu <- function() {
   )
 }
 
+# Feather Menu ----
+
+# Define Feather Menu
+feather_menu <- function() {
+  feather_options <- c(
+    "Feather Store: Access a feather store like dataframe but read only", 
+    "Feather Metadata: Retrieve metadata about a feather file", 
+    "Read Feather: Read Feather Files", 
+    "Write Feather: Write Feather Files", 
+    "Back")
+  feather_selection <- menu(feather_options, title = "Please select an action with Feather:")
+  
+  # Define Options in the Feather Mneu
+  feather_selected_option <- switch(
+    feather_selection,
+    "feather::feather(path = \"xxxxx\")",
+    "feather::feather_metadata(path = \"xxxxx\")",
+    "feather::read_feather(path = \"xxxxx\")",
+    "feather::write_feather(path = \"xxxxx\")",
+    return(sub_menu())  
+  )
+  
+  # Define Comments for the Selected Option
+  feather_selected_action <- switch(
+    feather_selection,
+    "# Accessed Feather Store",
+    "# Accessed Metadata",
+    "# Read Feather File(s)",
+    "# Wrote Feather File(s)"
+  )  
+  
+  clipr::write_clip(feather_selected_action)
+  clipr::read_clip() %>% 
+    write(file = "clips.txt", sep = "\n",  append = TRUE)
+  message("Comment Placed")
+  message(cli::rule())
+  
+  clipr::write_clip(selected_option)
+  message("Selected Option Has Been Copied")
+  message(cli::rule())
+  
+  clipr::read_clip() %>% 
+    write(file = "clips.txt", sep = "\n",  append = TRUE)
+  message("File Updated")
+}
+
+main_menu()
 
 
