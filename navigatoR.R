cb3e052aa7ee7e0540d4ca248289ec4185fde151
Kaya Ozkur <kayaozkur@gmail.com>
2023-08-08 19:43:15 -0500

    adding arrow I/O functionality, work in progress

--- navigatoR.R
+++ navigatoR.R
@@ -6,8 +6,7 @@ main_menu <- function() {
   selection <- menu(option, title = "Select an Action:")
 
   # Define Options in the Main Menu
-  selected_option <- switch(
-    selection,
+  selected_option <- switch(selection,
     sub_menu(),
     return(print("No Action Performed"))
   )
@@ -17,15 +16,21 @@ main_menu <- function() {
 
 # Define Sub Menu
 sub_menu <- function() {
-  sub_option <- c("Feather Files", "JSON Files", "Delimited Files (Vroom)", "Back")
+  sub_option <- c(
+    "Feather Files",
+    "JSON Files",
+    "Delimited Files (Vroom)",
+    "Arrow or Parquet Files (Arrow)",
+    "Back"
+  )
   sub_selection <- menu(sub_option, title = "Select a File Type:")
 
   # Define Options in the Sub Menu
-  selected_option <- switch(
-    sub_selection,
+  selected_option <- switch(sub_selection,
     feather_menu(),
     json_lite_menu(),
     vroom_io_menu(),
+    arrow_menu(),
     return(main_menu())
   )
 }
@@ -35,35 +40,34 @@ sub_menu <- function() {
 # Define Feather Menu
 feather_menu <- function() {
   feather_option <- c(
-    "Feather Store: Access a feather store like dataframe but read only",
-    "Feather Metadata: Retrieve metadata about a feather file",
-    "Read Feather: Read Feather Files",
-    "Write Feather: Write Feather Files",
-    "Back")
+    "Feather Store: Access a Feather Store like Dataframe but Read Only",
+    "Feather Metadata: Retrieve Metadata about a Feather File",
+    "Read Feather: Read Feather File",
+    "Write Feather: Write Feather File",
+    "Back"
+  )
   feather_selection <- menu(feather_option, title = "Please select an action with feather:")
 
   # Define Options in the Feather Menu
-  feather_selected_option <- switch(
-    feather_selection,
-    "feather::feather(path = \"xxxxx\")",
-    "feather::feather_metadata(path = \"xxxxx\")",
-    "feather::read_feather(path = \"xxxxx\")",
-    "feather::write_feather(path = \"xxxxx\")",
+  feather_selected_option <- switch(feather_selection,
+    "feather::feather(path = \"xxxxx.feather\")",
+    "feather::feather_metadata(path = \"xxxxx.feather\")",
+    "feather::read_feather(path = \"xxxxx.feather\")",
+    "feather::write_feather(path = \"xxxxx.feather\")",
     return(sub_menu())
   )
 
   # Define Comments for the Selected Option
-  feather_selected_action <- switch(
-    feather_selection,
+  feather_selected_action <- switch(feather_selection,
     "# Accessed Feather Store",
     "# Accessed Metadata",
-    "# Read Feather File(s)",
-    "# Wrote Feather File(s)"
+    "# Read Feather File",
+    "# Wrote Feather File"
   )
 
   clipr::write_clip(feather_selected_action)
   clipr::read_clip() %>%
-    write(file = "clips.txt", sep = "\n",  append = TRUE)
+    write(file = "clips.txt", sep = "\n", append = TRUE)
   message("Comment Placed")
   message(cli::rule())
 
@@ -72,7 +76,7 @@ feather_menu <- function() {
   message(cli::rule())
 
   clipr::read_clip() %>%
-    write(file = "clips.txt", sep = "\n",  append = TRUE)
+    write(file = "clips.txt", sep = "\n", append = TRUE)
   message("File Updated")
 }
 
@@ -83,16 +87,16 @@ feather_menu <- function() {
 # Define json_lite Menu
 json_lite_menu <- function() {
   json_lite_option <- c(
-    "Read JSON: Read JSON Files",
-    "Write JSON: Write JSON Files",
+    "Read JSON: Read JSON File",
+    "Write JSON: Write JSON File",
     "Convert R to JSON: Convert an R Object into JSON",
     "Convert JSON to R: Convert a JSON Object into R",
-    "Back")
+    "Back"
+  )
   json_lite_selection <- menu(json_lite_option, title = "Please select an action with jsonlite:")
 
   # Define Options in the json_lite Menu
-  json_lite_selected_option <- switch(
-    json_lite_selection,
+  json_lite_selected_option <- switch(json_lite_selection,
     "jsonlite::read_json(path = \"xxxxx.json\", simplifyVector = TRUE)",
     "jsonlite::write_json(path = \"xxxxx.json\")",
     "jsonlite::toJSON(pretty = TRUE)",
@@ -101,17 +105,16 @@ json_lite_menu <- function() {
   )
 
   # Define Comments for the Selected Option
-  json_lite_selected_action <- switch(
-    json_lite_selection,
+  json_lite_selected_action <- switch(json_lite_selection,
     "# Read JSON File",
     "# Wrote JSON File",
-    "# Converted R Object(s) to JSON",
-    "# Coonverted JSON Object(s) to R"
+    "# Converted R Object to JSON",
+    "# Coonverted JSON Object to R"
   )
 
   clipr::write_clip(json_lite_selected_action)
   clipr::read_clip() %>%
-    write(file = "clips.txt", sep = "\n",  append = TRUE)
+    write(file = "clips.txt", sep = "\n", append = TRUE)
   message("Comment Placed")
   message(cli::rule())
 
@@ -120,15 +123,15 @@ json_lite_menu <- function() {
   message(cli::rule())
 
   clipr::read_clip() %>%
-    write(file = "clips.txt", sep = "\n",  append = TRUE)
+    write(file = "clips.txt", sep = "\n", append = TRUE)
   message("File Updated")
 }
 
 
 # vroom Menu ----
 
-# Define vroom Menu
-vroom_io_menu<- function() {
+# Define vroom IO Menu
+vroom_io_menu <- function() {
   vroom_io_option <- c(
     "Read Delimited File: Read Delimited File into a Tibble",
     "Read Fixed Width File: Read a Fixed Width File into a Tibble",
@@ -137,16 +140,16 @@ vroom_io_menu<- function() {
     "Specify Column Positions: Paired Vectors of Start and End Positions",
     "Specify Column Positions: Named Arguments with Start and End Positions",
     "Specify Column Positions: Named Arguments with Column Widths",
-    "Write Rectangular Files: Write a Dataframe to a Delimited File",
-    "Write Rectangular Files: Write Lines to a File",
-    "Write Rectangular Files: Convert Data Frame to a Delimited String",
-    "Back")
+    "Write Rectangular File: Write a Dataframe to a Delimited File",
+    "Write Rectangular File: Write Lines to a File",
+    "Write Rectangular File: Convert Data Frame to a Delimited String",
+    "Back"
+  )
 
   vroom_io_selection <- menu(vroom_io_option, title = "Please select an action with jsonlite:")
 
-  # Define Options in the json_lite Menu
-  vroom_io_selected_option <- switch(
-    vroom_io_selection,
+  # Define Options in the vroom IO Menu
+  vroom_io_selected_option <- switch(vroom_io_selection,
     "vroom::vroom(file = \"xxxxx.csv_txt\")",
     "vroom::vroom_fwf(file = \"xxxxx.csv_txt\")",
     "vroom::vroom_fwf(file = \"xxxxx.csv_txt\", vroom::fwf_empty(file = \"xxxxx.csv_txt\", col_names = c(\"col_name1\", \"col_name2\", \"col_name3\")))",
@@ -161,15 +164,14 @@ vroom_io_menu<- function() {
   )
 
   # Define Comments for the Selected Option
-  vroom_io_selected_action <- switch(
-    vroom_io_selection,
+  vroom_io_selected_action <- switch(vroom_io_selection,
     "Read Delimited File into a Tibble",
     "Read a Fixed Width File into a Tibble",
-    "Specified Columns",
-    "Specified Columns",
-    "Specified Columns",
-    "Specified Columns",
-    "Specified Columns",
+    "Specified Columns Guessing Based on Col Positions",
+    "Specified Columns Col Length and Col Names",
+    "Specified Columns Start End Position and Col Names",
+    "Specified Columns Start End Position and Col Names",
+    "Specified Columns Col Length and Col Names",
     "Wrote a Dataframe to a Delimited File",
     "Wrote Lines to a File",
     "Converted Data Frame to a Delimited String"
@@ -177,7 +179,7 @@ vroom_io_menu<- function() {
 
   clipr::write_clip(vroom_io_selected_action)
   clipr::read_clip() %>%
-    write(file = "clips.txt", sep = "\n",  append = TRUE)
+    write(file = "clips.txt", sep = "\n", append = TRUE)
   message("Comment Placed")
   message(cli::rule())
 
@@ -186,14 +188,80 @@ vroom_io_menu<- function() {
   message(cli::rule())
 
   clipr::read_clip() %>%
-    write(file = "clips.txt", sep = "\n",  append = TRUE)
+    write(file = "clips.txt", sep = "\n", append = TRUE)
   message("File Updated")
 }
 
 
+# arrow Menu ----
+
+# Define arrow Menu
+arrow_menu <- function() {
+  arrow_option <- c(
+    "Read Arrow File: Read a Feather File",
+    "Read Arrow File: Read a Arrow IPC (aka Feather) File",
+    "Read Arrow File: Read Arrow IPC Stream Format",
+    "Read Arrow File: Read a Parquet File",
+    "Read Arrow File: Read a CSV File with Arrow",
+    "Read Arrow File: Read a TSV File with Arrow",
+    "Read Arrow File: Read a JSON File",
+    "Write Arrow File: Write a Feather File",
+    "Write Arrow File: Write a Arrow IPC (aka Feather) File",
+    "Write Arrow File: Write Arrow IPC Stream Format",
+    "Write Arrow File: Write Arrow Data to Raw Vector",
+    "Write Arrow File: Write a Parquet File",
+    "Write Arrow File: Write a CSV File with Arrow",
+    "Back"
+  )
+
+  arrow_selection <- menu(arrow_option, title = "Please select an action with jsonlite:")
+
+  # Define Options in the json_lite Menu
+  arrow_selected_option <- switch(arrow_selection,
+    "arrow::read_feather(file = \"xxxxx.feather\", col_select = NULL, as_data_frame = TRUE, mmap = TRUE) #mmap is for memory maping the file",
+    "arrow::read_ipc_file(file = \"xxxxx.arrow\", col_select = NULL, as_data_frame = TRUE, mmap = TRUE) #mmap is for memory maping the file",
+    "arrow::read_ipc_stream(file = \"xxxxx\", col_select = NULL, as_data_frame = TRUE, mmap = TRUE) #mmap is for memory maping the file",
+    "arrow::read_parquet(file = \"xxxxx\", col_select = NULL, as_data_frame = TRUE, props = ParquetArrowReaderProperties$create())",
+    "arrow::read_csv_arrow(file = \"xxxxx.csv\", escape_double = TRUE, escape_backslash = FALSE, schema = NULL, col_names = TRUE, col_types = NULL, col_select = NULL, na = c(\"\", \"NA\"), quoted_na = TRUE, skip_empty_rows = TRUE, skip = 0L)",
+    "arrow::read_tsv_arrow(file = \"xxxxx.tsv\", escape_double = TRUE, escape_backslash = FALSE, schema = NULL, col_names = TRUE, col_types = NULL, col_select = NULL, na = c(\"\", \"NA\"), quoted_na = TRUE, skip_empty_rows = TRUE, skip = 0L)",
+
+    return(sub_menu())
+  )
+
+  # Define Comments for the Selected Option
+  arrow_selected_action <- switch(arrow_selection,
+    "Read a Feather File",
+    "Read a Arrow IPC (aka Feather) File",
+    "Read Arrow IPC Stream Format",
+    "Read a Parquet File",
+    "Read a Delimited File with Arrow",
+    "Read a CSV File with Arrow",
+    "Read a TSV File with Arrow",
+    "Wrote a Feather File",
+    "Wrote a Arrow IPC (aka Feather) File",
+    "Wrote Arrow IPC Stream Format",
+    "Wrote Arrow Data to Raw Vector",
+    "Wrote a Parquet File",
+    "Wrote a CSV File with Arrow"
+  )
+
+  clipr::write_clip(arrow_selected_action)
+  clipr::read_clip() %>%
+    write(file = "clips.txt", sep = "\n", append = TRUE)
+  message("Comment Placed")
+  message(cli::rule())
+
+  clipr::write_clip(arrow_selected_option)
+  message("Selected Option Has Been Copied")
+  message(cli::rule())
+
+  clipr::read_clip() %>%
+    write(file = "clips.txt", sep = "\n", append = TRUE)
+  message("File Updated")
+}
+
 
 main_menu()
-
 
 
 
